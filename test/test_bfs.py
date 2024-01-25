38af9741c32bfcbff5956a1d6c5e05f5a30af0d7
David Gomez Siu <dgomezsiu@berkeley.edu>
2024-01-24 20:41:15 -0800

    writing bfs traversal test

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -10,7 +10,20 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
-    pass
+    # function to initiate a graph object with tiny network adj list
+    def test_tiny_network():
+        return Graph('data/tiny_network.adjlist')
+    
+    #test traversal count by initiating tiny network adj graph
+
+    def testing_traversal(test_graph):
+        start_node = 'Luke Gilbert'
+        expected_nodes = 30
+
+        visited_nodes = test_tiny_network.bfs(start_node)
+        assert len(visited_nodes) == expected_nodes, "Unexpected number of nodes"
+
+
 
 def test_bfs():
     """
