80f95413dda6e6e5b58e49ba5f6af26124a6aa3a
David Gomez Siu <dgomezsiu@berkeley.edu>
2024-01-24 21:59:57 -0800

    write new tests for traversal

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,6 +1,6 @@
 # write tests for bfs
 import pytest
-import networkx as ns
+import networkx as nx
 from search import graph
 
 def test_bfs_traversal():
@@ -11,9 +11,18 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
+
+    #starting at the first given node in the file, is the length of the number of nodes correct?
     test_graph = graph.Graph('data/tiny_network.adjlist')
     bfs_test_graph = test_graph.bfs('Luke Gilbert')
     assert len(bfs_test_graph) == 30
+
+    #use nx package to build bfs and compare graphs to make sure they are the same length
+    realgraph_bfs = nx.bfs_tree(g.graph, 'Luke Gilbert').nodes()
+    assert len(bfs_test_graph) == len(bfs_test_graph)
+
+    #check if the nodes are traversed in the same order
+    assert realgraph_bfs = bfs_test_graph
 
 
 def test_bfs():
